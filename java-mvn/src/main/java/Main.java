4f1338d46e700b8e609380972e53a01e80438baa
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-08-25 16:47:07 +0200

    Streamline

--- java-mvn/src/main/java/Main.java
+++ java-mvn/src/main/java/Main.java
@@ -1,22 +1,18 @@
 import com.github.fwilhe.inzell.*;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
     public static double foo(int i) {
         return Math.sqrt(i);
     }
 
     public static void main(String[] args) {
-        List<Column> l = new LinkedList<>();
-        l.add(new Column("sqrt", Main::foo));
-        l.add(new Column("cosine", StdlibKt::cosine));
-        l.add(new Column("powerOfTwo", StdlibKt::powerOfTwo));
-        l.add(new Column("random", StdlibKt::random));
 
-        Sheet s = SheetKt.javaSpreadsheetBuilder(l, "abc");
-
-        new CsvPrinter(s).printToStandardOut();
+        new CsvPrinter(
+                new SpreadsheetBuilder()
+                        .setCaption("test")
+                        .addColumn(new Column("sqrt", Main::foo))
+                        .addColumn(new Column("cosine", StdlibKt::cosine))
+                        .build())
+                .printToStandardOut();
     }
 }
