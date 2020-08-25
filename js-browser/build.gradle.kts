748ddeb20dbd85fd14026e0f791e7b3535a92d69
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-08-25 08:39:20 +0200

    Add browser example

---
+++ js-browser/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("js") version "1.4.0"
+}
+group = "me.florian"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    jcenter()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlinx")
+    }
+}
+dependencies {
+    testImplementation(kotlin("test-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
+    implementation("com.github.fwilhe.inzell:Inzell-js:0.1.0-SNAPSHOT")
+}
+kotlin {
+    js {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+    }
+}
