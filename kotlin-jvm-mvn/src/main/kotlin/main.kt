1bad4a0e910477040381948dd9b3af3f648dcbda
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-08-29 22:13:33 +0200

    fix-kt

--- kotlin-jvm-mvn/src/main/kotlin/main.kt
+++ kotlin-jvm-mvn/src/main/kotlin/main.kt
@@ -1,23 +1,23 @@
 import com.github.fwilhe.inzell.*
 
 fun main() {
-    val numberOfCpus = Column("Number of CPUs", ::powerOfTwo)
-    val nX = Column("Problem Size X-Dimension") { 100.0 }
-    val nY = Column("Problem Size Y-Dimension") { 100.0 }
-    val tA = Column("Calculation Time per Cell") { 10.0 }
-    val numberOfOperations = Column("Number of Operations") { 1.0 }
-    val tK = Column("Communication Time per Cell") { 200.0 }
-    fun timeParallel(x: Int): Double = (nX.eval(x) / numberOfCpus.eval(x)) * // Slice for each CPU
-            nY.eval(x) * // Whole Y-Dimension of the problem
-            tA.eval(x) * numberOfOperations.eval(x) + // Time to calculate each cell
-            tK.eval(x) * numberOfCpus.eval(x) // Communication increases with number of CPUs
+    val numberOfCpus = Column("Number of CPUs") { x -> x * x }
+    val nX = Column("Problem Size X-Dimension") { 100 }
+    val nY = Column("Problem Size Y-Dimension") { 100 }
+    val tA = Column("Calculation Time per Cell") { 10 }
+    val numberOfOperations = Column("Number of Operations") { 1 }
+    val tK = Column("Communication Time per Cell") { 200 }
+    fun timeParallel(x: Int): Int = (nX.evalInt(x) / numberOfCpus.evalInt(x) as Int) * // Slice for each CPU
+            nY.evalInt(x) * // Whole Y-Dimension of the problem
+            tA.evalInt(x) * numberOfOperations.evalInt(x) + // Time to calculate each cell
+            tK.evalInt(x) * numberOfCpus.evalInt(x) // Communication increases with number of CPUs
 
     val tP = Column("Parallel Time", ::timeParallel)
-    fun timeSequential(x: Int): Double = nX.eval(x) * nY.eval(x) * tA.eval(x) * numberOfOperations.eval(x)
+    fun timeSequential(x: Int): Int = nX.evalInt(x) * nY.evalInt(x) * tA.evalInt(x) * numberOfOperations.evalInt(x)
     val tS = Column("Sequential Time", ::timeSequential)
-    fun calculateSpeedup(x: Int): Double = timeSequential(x) / timeParallel(x)
+    fun calculateSpeedup(x: Int): Double = timeSequential(x) / timeParallel(x).toDouble()
     val speedup = Column("Speedup", ::calculateSpeedup)
-    fun calculateEfficiency(x: Int): Double = calculateSpeedup(x) / numberOfCpus.eval(x)
+    fun calculateEfficiency(x: Int): Double = calculateSpeedup(x) / numberOfCpus.evalInt(x)
     val efficiency = Column("Efficiency", ::calculateEfficiency)
 
     val sheet = spreadsheet {
@@ -33,7 +33,7 @@ fun main() {
         add(speedup)
         add(efficiency)
     }
-
-
     CsvPrinter(sheet).printToStandardOut()
+    MarkdownPrinter(sheet).printToStandardOut()
+    HtmlPrinter(sheet).printToStandardOut()
 }
